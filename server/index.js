5d990f63c22c5fde2b63bd7024c8c094907281fe
aarynverma <18100btcse02667@svvvindore.onmicrosoft.com>
2023-04-13 19:01:53 +0530

    Backend Setup

--- server/index.js
+++ server/index.js
@@ -9,7 +9,11 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -39,8 +43,11 @@ const upload = multer({ storage });
 // ROUTES WITH FILE
 
 app.post("auth/register", upload.single("picture"), register);
-
+app.post("posts", verifyToken, upload.single("picture"), createPost);
+// ROUTES
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 // MONGOOSE SETUP
 
 const PORT = process.env.PORT || 6001;
